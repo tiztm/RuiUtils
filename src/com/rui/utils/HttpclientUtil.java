0cea429fda6c1e6d7702e3c2c79b208704afed7d
nilszhang@gmail.com <nilszhang@gmail.com>
2016-02-14 14:22:08 +0800

    优化封装HttpClient的功能

--- src/com/rui/utils/HttpclientUtil.java
+++ src/com/rui/utils/HttpclientUtil.java
@@ -10,6 +10,8 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.client.params.ClientPNames;
+import org.apache.http.client.params.CookiePolicy;
 import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.ssl.SSLSocketFactory;
@@ -31,6 +33,7 @@ import java.security.cert.CertificateException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
 * Httpclient 4.0 工具包装类
@@ -60,271 +63,163 @@ public class HttpclientUtil {
 		DefaultHttpClient httpclient = new DefaultHttpClient();
 		httpclient.getParams().setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
 		//模拟浏览器，解决一些服务器程序只允许浏览器访问的问题
-		httpclient.getParams().setParameter(CoreProtocolPNames.USER_AGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)");
+		httpclient.getParams().setParameter(CoreProtocolPNames.USER_AGENT, "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36");
 		httpclient.getParams().setParameter(CoreProtocolPNames.USE_EXPECT_CONTINUE, Boolean.FALSE);
 		httpclient.getParams().setParameter(CoreProtocolPNames.HTTP_CONTENT_CHARSET, charset == null ? CHARSET_GBK : charset);
+		httpclient.getParams().setParameter(ClientPNames.COOKIE_POLICY, CookiePolicy.BEST_MATCH);
 		httpclient.setHttpRequestRetryHandler(requestRetryHandler);
 		return httpclient;
 	}
 
-
-
-	/**
-	* Get方式提交,URL中包含查询参数
-	* 
-	* @param url
-	* 提交地址
-	* @return 响应消息
-	 * @throws Exception
-	*/
 	public static String get(String url) throws Exception {
-		return get(url, null,null, null);
+		return get(url, null,null);
 	}
-	/**
-	* Get方式提交,URL中不包含查询参数
-	* 
-	* @param url
-	* 提交地址
-	* @param params
-	* 查询参数集, 键/值对
-	* @return 响应消息
-	 * @throws Exception
-	*/
+
 	public static String get(String url, Map<String, String> params) throws Exception {
 		return get(url, params,null, null);
 	}
-	
 
-
-	/**
-	* Get方式提交,URL中不包含查询参数，可设置字符集
-	*
-	* @param url
-	* 提交地址
-	* @param params
-	* 查询参数集, 键/值对
-	* @return 响应消息
-	 * @throws Exception
-	*/
 	public static String get(String url, Map<String, String> params,String charset) throws Exception {
 		return get(url, params,charset, null);
 	}
 
+	public static String get(String url, Map<String, String> params,String charset, HttpClient httpclient) throws Exception {
+		return get(url, params,charset, httpclient,null);
+	}
+
+
+	public static String post(String url, Map<String, String> params) throws Exception {
+		return post(url, params,null);
+	}
+
+	public static String post(String url, Map<String, String> params,String charset) throws Exception {
+		return post(url, params,charset, null);
+	}
+
+	public static String post(String url, Map<String, String> params, String charset,HttpClient httpclient) throws Exception {
+		return post(url, params,charset, httpclient,null);
+	}
+
+
+	public static String get(String url, Map<String, String> params,String charset, HttpClient httpclient,Map<String,String> headMap) throws Exception {
+
+		return request(url, params,charset, httpclient,headMap,"get");
+	}
+
+
+	public static String post(String url, Map<String, String> params, String charset,HttpClient httpclient,Map<String,String> headMap) throws Exception {
+
+		return request(url, params,charset, httpclient,headMap,"post");
+	}
+
+
+
 
 	/**
-	* Get方式提交,URL中不包含查询参数, 可设置字符集和对应使用的httpclient
-	* 
-	* @param url
-	* 提交地址
-	* @param params
-	* 查询参数集, 键/值对
-	* @param charset
-	* 参数提交编码集
-	* @return 响应消息
+	 * 使用get/POST的方式访问指定的url，返回响应信息
+	 * @param url 待访问的url
+	 * @param params 参数的map
+	 * @param charset 字符集
+	 * @param httpclient 特地的hc
+	 * @param headMap 指定的头部
+	 * @param requestMethod  GET or POST
+	 * @return 响应信息
 	 * @throws Exception
-	*/
-	public static String get(String url, Map<String, String> params,String charset, HttpClient httpclient) throws Exception {
+	 */
+	private static String request(String url, Map<String, String> params, String charset,HttpClient httpclient,Map<String,String> headMap,String requestMethod) throws Exception {
+
 		if (url == null || StringUtil.isEmpty(url)) {
 			return null;
 		}
-		List<NameValuePair> qparams = getParamsList(params);
-		if (qparams != null && qparams.size() > 0) {
-			charset = (charset == null ? CHARSET_GBK : charset);
-			String formatParams = URLEncodedUtils.format(qparams, charset);
-			url = (url.indexOf("?")) < 0 ? (url + "?" + formatParams) : (url
-					.substring(0, url.indexOf("?") + 1) + formatParams);
-		}
+
+		// 创建HttpClient实例
 		boolean isDefaultHc = false;
 		if(httpclient==null)
 		{
 			httpclient = getDefaultHttpClient(null);
 			isDefaultHc = true;
 		}
-		HttpGet hg = new HttpGet(url);
-
-
-
-//		hg.setHeader("Referer", "http://yun.baidu.com/share/home?uk=1075874930&view=share");
-//		hg.setHeader("X-Requested-With", "XMLHttpRequest");
-//		hg.setHeader("Host", "yun.baidu.com");
-//		hg.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36");
-
-
-		// 发送请求，得到响应
-		String responseStr = null;
-		try {
-			responseStr = httpclient.execute(hg, responseHandler);
-		} catch (ClientProtocolException e) {
-			throw new Exception("客户端连接协议错误", e);
-		} catch (IOException e) {
-			throw new Exception("IO操作异常", e);
-		} finally {
-			if(isDefaultHc)
-			{
-			abortConnection(hg, httpclient);
-			}
-		}
-		return responseStr;
-	}
-	/**
-	* Post方式提交,URL中不包含提交参数,
-	* 
-	* @param url
-	* 提交地址
-	* @param params
-	* 提交参数集, 键/值对
-	* @return 响应消息
-	 * @throws Exception
-	*/
-	public static String post(String url, Map<String, String> params) throws Exception {
-		return post(url, params,null, null);
-	}
-	
-	
-	
-	/**
-	* Post方式提交,URL中不包含提交参数 ，可设置字符集
-	* 
-	* @param url
-	* 提交地址
-	* @param params
-	* 提交参数集, 键/值对
-	* @return 响应消息
-	 * @throws Exception
-	*/
-	public static String post(String url, Map<String, String> params,String charset) throws Exception {
-		return post(url, params,charset, null);
-	}
-	
-	
-	/**
-	* Post方式提交,URL中不包含提交参数,可设置字符集和对应使用的httpclient
-	* 
-	* @param url
-	* 提交地址
-	* @param params
-	* 提交参数集, 键/值对
-	* @param charset
-	* 参数提交编码集
-	* @return 响应消息
-	 * @throws Exception
-	*/
-	public static String post(String url, Map<String, String> params, String charset,HttpClient httpclient) throws Exception {
-		boolean isDefaultHc = false;
-		if (url == null || StringUtil.isEmpty(url)) {
-			return null;
-		}
-		// 创建HttpClient实例
-		if(httpclient==null)
-		{
-		 httpclient = getDefaultHttpClient(null);
-		 isDefaultHc = true;
-		}
-		
-		
-		UrlEncodedFormEntity formEntity = null;
-		try {
-			if (charset == null || StringUtil.isEmpty(charset)) {
-				formEntity = new UrlEncodedFormEntity(getParamsList(params),CHARSET_UTF8);
-			} else {
-				formEntity = new UrlEncodedFormEntity(getParamsList(params), charset);
-			}
-		} catch (UnsupportedEncodingException e) {
-			throw new Exception("不支持的编码集", e);
-		}
-		HttpPost hp = new HttpPost(url); 
-		hp.setEntity(formEntity);
-		// 发送请求，得到响应
-		String responseStr = null;
-		try {
-			responseStr = httpclient.execute(hp, responseHandler);
-		} catch (ClientProtocolException e) {
-			throw new Exception("客户端连接协议错误", e);
-		} catch (IOException e) {
-			throw new Exception("IO操作异常", e);
-		} finally {
-			if(isDefaultHc)
-			{
-			abortConnection(hp, httpclient);
-			}
-		}		
-		return responseStr;
-	}
-	/**
-	* Post方式提交,忽略URL中包含的参数,解决SSL双向数字证书认证
-	* 
-	* @param url
-	* 提交地址
-	* @param params
-	* 提交参数集, 键/值对
-	* @param charset
-	* 参数编码集
-	* @param keystoreUrl
-	* 密钥存储库路径
-	* @param keystorePassword
-	* 密钥存储库访问密码
-	* @param truststoreUrl
-	* 信任存储库绝路径
-	* @param truststorePassword
-	* 信任存储库访问密码, 可为null
-	* @return 响应消息
-	* @throws Exception
-	*/
-	public static String post(String url, Map<String, String> params,String charset, HttpClient httpclient, final URL keystoreUrl, 
-			final String keystorePassword, final URL truststoreUrl,	final String truststorePassword) throws Exception {
-		if (url == null || StringUtil.isEmpty(url)) {
-			return null;
-		}
-		boolean isDefaultHc =false;
-		if(httpclient==null)
-		{
-			isDefaultHc =true;
-		 httpclient = getDefaultHttpClient(null);
-		}
-		UrlEncodedFormEntity formEntity = null;
-		try {
-			if (charset == null || StringUtil.isEmpty(charset)) {
-				formEntity = new UrlEncodedFormEntity(getParamsList(params),CHARSET_UTF8);
-			} else {
-				formEntity = new UrlEncodedFormEntity(getParamsList(params), charset);
-			}
-		} catch (UnsupportedEncodingException e) {
-			throw new Exception("不支持的编码集", e);
-		}
+		HttpGet hg = null;
 		HttpPost hp = null;
-		String responseStr = null;
-		try {
-			KeyStore keyStore = createKeyStore(keystoreUrl, keystorePassword);
-			KeyStore trustStore = createKeyStore(truststoreUrl, keystorePassword);
-			SSLSocketFactory socketFactory = new SSLSocketFactory(keyStore,	keystorePassword, trustStore);
-			Scheme scheme = new Scheme(SSL_DEFAULT_SCHEME, socketFactory, SSL_DEFAULT_PORT);
-			httpclient.getConnectionManager().getSchemeRegistry().register(scheme);
+		if(requestMethod.equals("get"))
+		{
+			List<NameValuePair> qparams = getParamsList(params);
+			if (qparams != null && qparams.size() > 0) {
+				charset = (charset == null ? CHARSET_GBK : charset);
+				String formatParams = URLEncodedUtils.format(qparams, charset);
+				url = (url.indexOf("?")) < 0 ? (url + "?" + formatParams) : (url
+						.substring(0, url.indexOf("?") + 1) + formatParams);
+			}
+
+			hg = new HttpGet(url);
+
+		}
+		else if(requestMethod.equals("post"))
+		{
+			UrlEncodedFormEntity formEntity = null;
+
+			if(params!=null) {
+				try {
+					if (charset == null || StringUtil.isEmpty(charset)) {
+						formEntity = new UrlEncodedFormEntity(getParamsList(params), CHARSET_UTF8);
+					} else {
+						formEntity = new UrlEncodedFormEntity(getParamsList(params), charset);
+					}
+				} catch (UnsupportedEncodingException e) {
+					throw new Exception("不支持的编码集", e);
+				}
+			}
+
 			hp = new HttpPost(url);
 			hp.setEntity(formEntity);
-			responseStr = httpclient.execute(hp, responseHandler);			
-		} catch (NoSuchAlgorithmException e) {
-			throw new Exception("指定的加密算法不可用", e);
-		} catch (KeyStoreException e) {
-			throw new Exception("keytore解析异常", e);
-		} catch (CertificateException e) {
-			throw new Exception("信任证书过期或解析异常", e);
-		} catch (FileNotFoundException e) {
-			throw new Exception("keystore文件不存在", e);
+
+		}
+
+		if(headMap!=null)
+		{
+			Set<String> strings = headMap.keySet();
+			for(String s:strings)
+			{
+				if( hg != null)
+				{
+					hg.setHeader(s, headMap.get(s));
+				}
+				else if(hp != null)
+				{
+					hp.setHeader(s, headMap.get(s));
+				}
+			}
+		}
+
+
+		// 发送请求，得到响应
+		String responseStr = null;
+		try {
+			if( hg != null)
+			{
+				responseStr = httpclient.execute(hg, responseHandler);
+			}
+			else if(hp != null)
+			{
+				responseStr = httpclient.execute(hp, responseHandler);
+			}
+		} catch (ClientProtocolException e) {
+			throw new Exception("客户端连接协议错误", e);
 		} catch (IOException e) {
-			throw new Exception("I/O操作失败或中断 ", e);
-		} catch (UnrecoverableKeyException e) {
-			throw new Exception("keystore中的密钥无法恢复异常", e);
-		} catch (KeyManagementException e) {
-			throw new Exception("处理密钥管理的操作异常", e);
+			throw new Exception("IO操作异常", e);
 		} finally {
 			if(isDefaultHc)
 			{
-			abortConnection(hp, httpclient);
+				abortConnection(hg, httpclient);
 			}
 		}
 		return responseStr;
-	}
 
+
+
+
+
+	}
 
 
 	
@@ -345,33 +240,7 @@ public class HttpclientUtil {
 		}
 	}
 	
-	/**
-	* 从给定的路径中加载此 KeyStore
-	* 
-	* @param url
-	* keystore URL路径
-	* @param password
-	* keystore访问密钥
-	* @return keystore 对象
-	*/
-	private static KeyStore createKeyStore(final URL url, final String password)
-			throws KeyStoreException, NoSuchAlgorithmException,	CertificateException, IOException {
-		if (url == null) {
-			throw new IllegalArgumentException("Keystore url may not be null");
-		}
-		KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
-		InputStream is = null;
-		try {
-			is = url.openStream();
-			keystore.load(is, password != null ? password.toCharArray() : null);
-		} finally {
-			if (is != null){
-				is.close();
-				is = null;
-			}
-		}
-		return keystore;
-	}
+
 
 	/**
 	* 将传入的键/值对参数转换为NameValuePair参数集
